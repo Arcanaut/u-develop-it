e7ee44858a38d7f746dc88d5081f7d034c5869f8
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-01-07 14:03:50 -0600

    added voter routes

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 //requirements
 const express = require('express');
 const db = require('./db/connection');
-
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const apiRoutes = require('./routes/apiRoutes');
-
-//Express middleware
-app.use(express.urlencoded({
-    extended: false
-}));
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
 //Default response for any other request (Not Found)
@@ -24,3 +22,4 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
